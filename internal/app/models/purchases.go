7ea088d99ebfbd52dbea16c0eaaa8bc5bd425ff7
Wellington Reis <wellington.reis@redelojacorr.com.br>
2024-06-03 21:35:26 -0300

    Primeiro commit

--- internal/app/models/purchases.go
+++ internal/app/models/purchases.go
@@ -23,124 +23,108 @@ type CatalogCode struct {
 }
 
 type ItemPurchase struct {
-	IDCompra                      string  `json:"idCompra"`
-	IDItemCompra                  int     `json:"idItemCompra"`
-	Forma                         string  `json:"forma"`
-	Modalidade                    int     `json:"modalidade"`
-	CriterioJulgamento            string  `json:"criterioJulgamento"`
-	NumeroItemCompra              int     `json:"numeroItemCompra"`
-	DescricaoItem                 string  `json:"descricaoItem"`
-	CodigoItemCatalogo            int     `json:"codigoItemCatalogo"`
-	NomeUnidadeMedida             string  `json:"nomeUnidadeMedida"`
-	SiglaUnidadeMedida            string  `json:"siglaUnidadeMedida"`
-	NomeUnidadeFornecimento       string  `json:"nomeUnidadeFornecimento"`
-	SiglaUnidadeFornecimento      string  `json:"siglaUnidadeFornecimento"`
-	CapacidadeUnidadeFornecimento float64 `json:"capacidadeUnidadeFornecimento"`
-	Quantidade                    float64 `json:"quantidade"`
-	PrecoUnitario                 float64 `json:"precoUnitario"`
-	PercentualMaiorDesconto       float64 `json:"percentualMaiorDesconto"`
-	NIFornecedor                  string  `json:"niFornecedor"`
-	NomeFornecedor                string  `json:"nomeFornecedor"`
-	Marca                         string  `json:"marca"`
-	CodigoUasg                    string  `json:"codigoUasg"`
-	NomeUasg                      string  `json:"nomeUasg"`
-	CodigoMunicipio               int     `json:"codigoMunicipio"`
-	Municipio                     string  `json:"municipio"`
-	Estado                        string  `json:"estado"`
-	CodigoOrgao                   int     `json:"codigoOrgao"`
-	NomeOrgao                     string  `json:"nomeOrgao"`
-	Poder                         string  `json:"poder"`
-	Esfera                        string  `json:"esfera"`
-	DataCompra                    string  `json:"dataCompra"`
-	DataHoraAtualizacaoCompra     string  `json:"dataHoraAtualizacaoCompra"`
-	DataHoraAtualizacaoItem       string  `json:"dataHoraAtualizacaoItem"`
-	DataResultado                 string  `json:"dataResultado"`
-	DataHoraAtualizacaoUasg       string  `json:"dataHoraAtualizacaoUasg"`
+	IDCompra                      string    `json:"idCompra"`
+	IDItemCompra                  int       `json:"idItemCompra"`
+	Forma                         string    `json:"forma"`
+	Modalidade                    int       `json:"modalidade"`
+	CriterioJulgamento            string    `json:"criterioJulgamento"`
+	NumeroItemCompra              int       `json:"numeroItemCompra"`
+	DescricaoItem                 string    `json:"descricaoItem"`
+	CodigoItemCatalogo            int       `json:"codigoItemCatalogo"`
+	NomeUnidadeMedida             string    `json:"nomeUnidadeMedida"`
+	SiglaUnidadeMedida            string    `json:"siglaUnidadeMedida"`
+	NomeUnidadeFornecimento       string    `json:"nomeUnidadeFornecimento"`
+	SiglaUnidadeFornecimento      string    `json:"siglaUnidadeFornecimento"`
+	CapacidadeUnidadeFornecimento float64   `json:"capacidadeUnidadeFornecimento"`
+	Quantidade                    float64   `json:"quantidade"`
+	PrecoUnitario                 float64   `json:"precoUnitario"`
+	PercentualMaiorDesconto       float64   `json:"percentualMaiorDesconto"`
+	NIFornecedor                  string    `json:"niFornecedor"`
+	NomeFornecedor                string    `json:"nomeFornecedor"`
+	Marca                         string    `json:"marca"`
+	CodigoUasg                    string    `json:"codigoUasg"`
+	NomeUasg                      string    `json:"nomeUasg"`
+	CodigoMunicipio               int       `json:"codigoMunicipio"`
+	Municipio                     string    `json:"municipio"`
+	Estado                        string    `json:"estado"`
+	CodigoOrgao                   int       `json:"codigoOrgao"`
+	NomeOrgao                     string    `json:"nomeOrgao"`
+	Poder                         string    `json:"poder"`
+	Esfera                        string    `json:"esfera"`
+	DataCompra                    time.Time `json:"dataCompra"`
+	DataHoraAtualizacaoCompra     string    `json:"dataHoraAtualizacaoCompra"`
+	DataHoraAtualizacaoItem       string    `json:"dataHoraAtualizacaoItem"`
+	DataResultado                 string    `json:"dataResultado"`
+	DataHoraAtualizacaoUasg       string    `json:"dataHoraAtualizacaoUasg"`
 }
 
 func FilterDocumentsByPresentation(db *mongodb.MongoDB) ([]CatalogCode, error) {
 	collection := db.Client.Database("admin").Collection("purchases")
 	ctx := context.Background()
 
-	currentTime := time.Now()
+	now := time.Now().UTC()
+	year, month, day := now.Date()
+	today := time.Date(year, month, day, 0, 0, 0, 0, time.FixedZone("", -3*60*60))
 
-	threeMonthsAgo := currentTime.AddDate(0, -3, 0)
-	sixMonthsAgo := currentTime.AddDate(0, -6, 0)
-	twelveMonthsAgo := currentTime.AddDate(0, -12, 0)
+	threeMonthsAgo := today.AddDate(0, -3, 0)
+	sixMonthsAgo := today.AddDate(0, -6, 0)
+	twelveMonthsAgo := today.AddDate(0, -12, 0)
 
 	pipeline := mongo.Pipeline{
 		{{"$match", bson.M{"apresentacao": bson.M{"$exists": true}}}},
-		{{"$addFields", bson.M{
-			"resultadoFiltrado": bson.M{"$filter": bson.M{
+		{{"$addFields", bson.D{
+			{"resultadoFiltrado", bson.M{"$filter": bson.M{
 				"input": "$resultado",
 				"as":    "item",
 				"cond":  bson.M{"$eq": []interface{}{"$$item.nomeunidadefornecimento", "$apresentacao"}},
-			}},
-			"datacompra": bson.M{"$dateToString": bson.M{
-				"format": "%Y-%m-%d",
-				"date":   "$datacompra",
-			}},
+			}}},
 		}}},
 		{{"$match", bson.M{"resultadoFiltrado": bson.M{"$ne": []interface{}{}}}}},
-		{{"$match", bson.M{
-			"datacompra": bson.M{"$gte": threeMonthsAgo.Format("2006-01-02")},
-		}}},
-		{{"$group", bson.M{
-			"_id":   nil,
-			"count": bson.M{"$sum": 1},
-		}}},
-		{{"$addFields", bson.M{
-			"buscarSeVazio": bson.M{
-				"$cond": bson.M{
-					"if":   bson.M{"$eq": []interface{}{"$count", 0}},
-					"then": true,
-					"else": false,
+		{{"$addFields", bson.D{
+			{"resultadoFiltrado", bson.M{"$filter": bson.M{
+				"input": "$resultadoFiltrado",
+				"as":    "item",
+				"cond": bson.M{
+					"$or": []interface{}{
+						bson.M{
+							"$and": []interface{}{
+								bson.M{"$gte": []interface{}{"$$item.datacompra", threeMonthsAgo}},
+								bson.M{"$eq": []interface{}{"$buscarSeVazio", true}},
+							},
+						},
+						bson.M{
+							"$and": []interface{}{
+								bson.M{"$gte": []interface{}{"$$item.datacompra", sixMonthsAgo}},
+								bson.M{"$eq": []interface{}{"$buscarSeVazio", true}},
+							},
+						},
+						bson.M{"$gte": []interface{}{"$$item.datacompra", twelveMonthsAgo}},
+					},
 				},
-			},
-		}}},
-		{{"$match", bson.M{
-			"buscarSeVazio": true,
-			"datacompra":    bson.M{"$gte": sixMonthsAgo.Format("2006-01-02")},
-		}}},
-		{{"$group", bson.M{
-			"_id":   nil,
-			"count": bson.M{"$sum": 1},
-		}}},
-		{{"$addFields", bson.M{
-			"buscarSeVazio": bson.M{
-				"$cond": bson.M{
-					"if":   bson.M{"$eq": []interface{}{"$count", 0}},
-					"then": true,
-					"else": false,
-				},
-			},
-		}}},
-		{{"$match", bson.M{
-			"buscarSeVazio": true,
-			"datacompra":    bson.M{"$gte": twelveMonthsAgo.Format("2006-01-02")},
+			}}},
 		}}},
 	}
 
-	cur, err := collection.Aggregate(ctx, pipeline)
+	cursor, err := collection.Aggregate(ctx, pipeline)
 	if err != nil {
 		return nil, fmt.Errorf("error executing aggregate query: %v", err)
 	}
-	defer cur.Close(ctx)
+	defer cursor.Close(ctx)
 
 	var updatedDocuments []CatalogCode
 
-	var documents []CatalogCode
-	if err := cur.All(ctx, &documents); err != nil {
-		return nil, fmt.Errorf("error retrieving documents: %v", err)
-	}
-
-	for _, doc := range documents {
+	for cursor.Next(ctx) {
+		var doc CatalogCode
+		if err := cursor.Decode(&doc); err != nil {
+			return nil, fmt.Errorf("error decoding document: %v", err)
+		}
 
 		if _, err := collection.DeleteOne(ctx, bson.M{"catmat": doc.Catmat}); err != nil {
 			return nil, fmt.Errorf("error deleting document: %v", err)
 		}
 
 		doc.Resultado = doc.ResultadoFiltrado
+		doc.ResultadoFiltrado = nil
 
 		if _, err := collection.InsertOne(ctx, doc); err != nil {
 			return nil, fmt.Errorf("error inserting document: %v", err)
@@ -149,7 +133,7 @@ func FilterDocumentsByPresentation(db *mongodb.MongoDB) ([]CatalogCode, error) {
 		updatedDocuments = append(updatedDocuments, doc)
 	}
 
-	if err := cur.Err(); err != nil {
+	if err := cursor.Err(); err != nil {
 		return nil, fmt.Errorf("cursor error: %v", err)
 	}
 
